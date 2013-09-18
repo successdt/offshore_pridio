7a401d5e88c9c500956c8b8d94266090d26c6af5
thanhdd <thanhdd@lifetimetech.vn>
2013-09-18 11:48:38 +0700

    update footer

--- wp-content/themes/doover/footer.php
+++ wp-content/themes/doover/footer.php
@@ -13,23 +13,26 @@
 	<div class="Wrapper">
 	
 		<?php get_sidebar( 'footer' ); ?>
-		
+		<?php /*
 		<div class="row clearfix">
 			<div class="one_third col" style="min-height:300px;">
-				<?php /*
+				
 				<iframe src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fecomwebpro&amp;width=300&amp;height=258&amp;show_faces=true&amp;colorscheme=light&amp;stream=false&amp;show_border=false&amp;header=false&amp;appId=401582569900520" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:300px; height:258px;" allowTransparency="true">
 				</iframe>
-				*/?>
+
 			</div>
 			
 			<div class="one_third col">
+
 				<div class="copyrights">
 					<p class="author">&copy; <?php echo date( 'Y' ); ?> <span><?php bloginfo( 'name' ); ?></span>. <?php _e('All Rights Reserved. Powered by','doover'); ?> <a href="http://wordpress.org">WordPress</a>.</p>
 					<p><?php _e('Created by','doover'); ?> <a href="http://ecomwebpro.com">EWP</a>.</p>
 				</div>
+
 			</div>
 			
 			<div class="one_third col last">
+
 				<strong>Địa chỉ:</strong><br />
 				<strong>Ecom Web Pro</strong><br />
 				Tòa Nhà Việt Á - Duy Tân - Dịch Vọng Hậu - Cầu Giấy - Hà Nội<br />
@@ -51,15 +54,17 @@
 						</ul>
 					</li>
 				</ul>
-				<a class="back_to_top" href="#" title="<?php _e('Back to top','doover'); ?>"><?php _e('Back to top','doover'); ?></a>
+				
+				
 			</div>
 								
 		</div>
-
+		*/ ?>
 	</div>
 </footer>
 
 <?php wp_footer(); ?>
+<?php /*
 <script type="text/javascript">
 	var subiz_account_id = "1953";
 	(function() { var sbz = document.createElement("script");
@@ -68,6 +73,8 @@
 	sbz.src = ("https:" == document.location.protocol ? "https://" : "http://") + "widget.subiz.com/static/js/loader.js?v="+ (new Date()).getFullYear() + ("0" + ((new Date()).getMonth() + 1)).slice(-2) + ("0" + (new Date()).getDate()).slice(-2); 
 	var s = document.getElementsByTagName("script")[0];
 	s.parentNode.insertBefore(sbz, s);})();
-</script>	
+
+</script>
+*/ ?>	
 </body>
 </html>
